e70957e41fdef465ebdeaaedd53cad79d020b3ce
Aditya Tiwari <adityaat1902@gmail.com>
2023-04-20 17:54:56 +0530

    Add files via upload

---
+++ allstringmethods.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class allstringmethods {
+    public static void main(String[] args) {
+        // String para="hello i am Aditya Tiwari. I am a student of MCA.";
+
+        // String para1="welcome to UPES";
+
+        String s1="aditya";
+
+        // System.out.println("length of this string is : "+para.length());
+
+        // System.out.println("converting to upper case : "+para.toUpperCase());
+
+        // System.out.println("converting to lower case : "+para.toLowerCase());
+
+        // System.out.println("finding the index in a given string : "+para.indexOf("Aditya"));
+
+        // System.out.println("concating two strings : "+para.concat(para1));
+
+        // System.out.println("to find endinng of a string : "+para.endsWith("MCA"));
+
+        // System.out.println("to find whether the string is empty : "+para.isEmpty());
+
+        // System.out.println("to check stating of a string : "+para.startsWith("hello"));
+
+        // System.out.println("use of split : "+para.split(" "));
+        
+        System.out.println(s1.replace('a', 'b'));
+
+        // System.out.println(para.indexOf("t", 14));
+
+        // para=para.replace("MCA","MSC");
+
+        // System.out.println(para);
+    }
+}
